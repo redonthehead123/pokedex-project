3c0a8e166b42380ffbe01cd1ccd80f9d75c506ce
redonthehead123 <jordangreer1997@yahoo.com>
2025-05-19 15:25:00 -0500

    fixed brackets
    '{' and '[' were flip flopped in place of each other.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-let pokemonList = {
-    [name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']],
-    [name: 'Charmander', height: 0.6, types: ['fire']],
-    [name: 'Squirtle', height: 0.5, types: ['water']],
-    [name: 'caterpie', height: 0.3, types: ['bug']],
-    [name: 'weedle', height: 0.3, types: ['bug', 'poison']],
-    [name: 'pidgey', height: 0.3, types: ['normal', 'flying']],
-};
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']},
+    {name: 'caterpie', height: 0.3, types: ['bug']},
+    {name: 'weedle', height: 0.3, types: ['bug', 'poison']},
+    {name: 'pidgey', height: 0.3, types: ['normal', 'flying']},
+];
