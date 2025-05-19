e56fd0ee487be3c018282a26a59ba4066fb7c667
redonthehead123 <jordangreer1997@yahoo.com>
2025-05-19 15:20:45 -0500

    List Created
    Removed original code and added an array of pokemon and their stats.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = 'mac and cheese';
-document.write(favoriteFood);
+let pokemonList = {
+    [name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']],
+    [name: 'Charmander', height: 0.6, types: ['fire']],
+    [name: 'Squirtle', height: 0.5, types: ['water']],
+    [name: 'caterpie', height: 0.3, types: ['bug']],
+    [name: 'weedle', height: 0.3, types: ['bug', 'poison']],
+    [name: 'pidgey', height: 0.3, types: ['normal', 'flying']],
+};
