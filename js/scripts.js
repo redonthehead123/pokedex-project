6ce6aa7618eba8c68a705bd08ace9df2e8808ea8
redonthehead123 <jordangreer1997@yahoo.com>
2025-06-12 22:03:29 -0500

    Modals
    Removed console.log method to get pokemon info and added modal to display each pokemon's name, height, type and a picture.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let modalContainer = document.querySelector('#modal-container');
 
-    //Add a new Pokémon to the list
+    // Add a new Pokémon to the list
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
-    //returns all pokemon
+    // Returns all pokemon
     function getAll() {
         return pokemonList;
     }
@@ -33,7 +34,11 @@ let pokemonRepository = (function () {
 
     function showDetails(pokemon) {
       loadDetails(pokemon).then(function () {
-        console.log(pokemon);
+        showModal(
+          pokemon.name,
+          'Height: ' + pokemon.height + ', Types: ' + pokemon.types.map(type => type.type.name).join(', '),
+          pokemon.imageUrl
+        );
       });
     }
 
@@ -58,7 +63,6 @@ let pokemonRepository = (function () {
       return fetch(url).then(function (response) {
         return response.json();
       }).then(function (details) {
-        // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
@@ -67,7 +71,61 @@ let pokemonRepository = (function () {
       });
     }
 
-    //Keys to penetrate the IIFE
+    function showModal(title, text, imageUrl) {
+        // Clear all existing modal content
+        modalContainer.innerHTML = '';
+    
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+    
+        // Add the new modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+    
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+    
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        // Create and append the Pokémon image if imageUrl is provided
+        if (imageUrl) {
+          let imgElement = document.createElement('img');
+          imgElement.src = imageUrl;
+          imgElement.alt = title;
+          imgElement.style.display = 'block';
+          imgElement.style.margin = '0 auto 16px auto';
+          imgElement.style.maxWidth = '150px';
+          modal.appendChild(imgElement);
+        }
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+    
+        modalContainer.classList.add('is-visible');
+        modalContainer.addEventListener('click', (e) => {
+          let target = e.target;
+          if (target === modalContainer) {
+            hideModal();
+          }
+        });
+    }
+    
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+    
+  window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+        }
+    });
+
+    // Keys to penetrate the IIFE
     return {
         add: add,
         getAll: getAll,
@@ -78,7 +136,6 @@ let pokemonRepository = (function () {
 })();
 
 pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
   pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
   });
